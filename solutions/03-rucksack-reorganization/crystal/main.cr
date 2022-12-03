ddd90678c44bf64f80980d413ee3cd6b4ebd8647
Amy <amy@fluff.tech>
2022-12-03 01:21:44 -0500

    day 3 Crystal solution

---
+++ solutions/03-rucksack-reorganization/crystal/main.cr
@@ -0,0 +1,24 @@
+rucksacks = File.read_lines("input.txt").map(&.chars)
+
+PRIORITY_MAP = (('a'..'z').to_a + ('A'..'Z').to_a)
+	.map_with_index { |item, i| {item, i + 1} }
+	.to_h
+
+def intersect(sets)
+	sets.reduce { |acc, set| acc & set }.first
+end
+
+def priority(items)
+	items.map { |item| PRIORITY_MAP[item] }.sum
+end
+
+priority_items = rucksacks.map { |rucksack|
+	compartments = rucksack.in_groups_of(rucksack.size // 2)
+	intersect(compartments)
+}
+
+badges = rucksacks.in_groups_of(3, [] of Char)
+	.map { |group| intersect(group) }
+
+puts "Part 1 answer: #{priority(priority_items)}"
+puts "Part 2 answer: #{priority(badges)}"
