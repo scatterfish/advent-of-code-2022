353039357a7ed0b87afdc4f868c38f91e0333c9c
Amy <amy@fluff.tech>
2022-12-14 14:58:51 -0500

    tiny nitpick

--- solutions/01-calorie-counting/crystal/main.cr
+++ solutions/01-calorie-counting/crystal/main.cr
@@ -3,4 +3,4 @@ calories = File.read("input.txt").split("\n\n").map(&.split.sum(&.to_i))
 sorted = calories.sort.reverse
 
 puts "Part 1 answer: #{sorted.first}"
-puts "Part 2 answer: #{sorted[0..2].sum}"
+puts "Part 2 answer: #{sorted.first(3).sum}"
